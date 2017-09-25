927756fc0b141e8a3ad4aee738f0d07a4cb40988
hss1994 <1070798770@qq.com>
2017-09-25 18:08:21 +0800

    删除运费等多余代码

--- static/js/app/controller/share/ShareProduct.js
+++ static/js/app/controller/share/ShareProduct.js
@@ -40,14 +40,9 @@ define([
     	
     	$.when(
     		Ajax.get("808026",{"code":dCode}),
-    		base.getDictList("currency","802006"),
-    		Ajax.get("808917",{
-    			"key":"SP_YUNFEI",
-    			"systemCode": SYSTEM_CODE,
-    			"companyCode": COMPANY_CODE,
-    		})
-    	).then(function(res,dres,yres){
-			if(res.success && dres.success && yres.success){
+    		base.getDictList("currency","802006")
+    	).then(function(res,dres){
+			if(res.success && dres.success){
 				
 				var distRes = dres.data;
 				
@@ -56,10 +51,6 @@ define([
 				var dadvText = res.data.name;
 				var systemCode = res.data.systemCode;
 				var dslogan = res.data.slogan;
-				var dyunfei = yres.data.cvalue;
-//				var dprice1 = res.data.price1;
-//				var dprice2 = res.data.price2;
-//				var dprice3 = res.data.price3;
 				
 				dtemplateCode = res.data.code;
 				dtprice = (res.data.toAmount/1000).toFixed(2);
@@ -75,36 +66,11 @@ define([
 					s+="<img src='"+PIC_PREFIX+strs[i]+"'/>";
 				}
 				
-//				if(dprice1==0){
-//					dprice1="";
-//				}else if(dprice1!=0 && dprice2==0 && dprice3==0){
-//					dprice1= dprice1/1000 +"人民币";
-//				}else{
-//					dprice1= dprice1/1000 +"人民币"+"+";
-//				}
-//				
-//				if(dprice2==0){
-//					dprice2="";
-//				}else if(dprice2!=0 && dprice1==0 && dprice3==0){
-//					dprice2= dprice2/1000 +"购物币";
-//				}else{
-//					dprice2= dprice2/1000 +"购物币"+"+";
-//				}
-//				
-//				if(dprice3!=0){
-//					dprice3= dprice3/1000 +"钱包币"
-//				}else{
-//					dprice3="";
-//				}
-				
-				
 				$(".d-banner div img").attr("src",PIC_PREFIX+dadvpic+THUMBNAIL_SUFFIX)
 				$(".d-tit").html(dadvText);
 				$(".d-slogan").html(dslogan);
 				$(".s-img").html(base.loadImg(s));
 				$(".s-con").html(res.data.description)
-//				$(".s-price").html(dprice1+dprice2+dprice3)
-				$(".d-yunfei").html("运费："+dyunfei+"元");
 				
 			}else{
 				base.showMsg(res.msg)
